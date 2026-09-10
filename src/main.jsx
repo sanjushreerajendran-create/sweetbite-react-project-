20653438b318142c528ddedd5aedbeece4375430
Sanjushree Rajendran <sanjushreerajendran@gmail.com>
2026-09-10 11:11:20 +0530

    Fix Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import "./styles/products.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sweetbite-react-project-/">
       <ProductProvider>
         <App />
       </ProductProvider>
