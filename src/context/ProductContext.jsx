fee9f80a091f36ccedb895a5876d8711ed41a71f
Sanjushree Rajendran <sanjushreerajendran@gmail.com>
2026-09-10 11:31:30 +0530

    Fix useLocalStorage import

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import useLocalStorage from "../hooks/Uselocalstorage";
+import useLocalStorage from "../hooks/useLocalStorage.jsx";
 import {
   initialProducts,
   initialCategories,
